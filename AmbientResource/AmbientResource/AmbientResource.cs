7811334f29356b9342beac0be36d69db4035fc30
Nicolai Henriksen <nicolai.h80@gmail.com>
2016-12-06 21:56:12 +0100

    Initial commit

---
+++ AmbientResource/AmbientResource/AmbientResource.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Nicolai.Resources
+{
+    public class AmbientResource : IDisposable
+    {
+
+        public static Func<IDisposable> ResourceFactory { get; set; }
+
+        private static readonly object CurrentLock = new object();
+        private static AmbientResource _current;
+
+        public static AmbientResource Current
+        {
+            get
+            {
+                if (ResourceFactory != null)
+                {
+                    lock (CurrentLock)
+                    {
+                        if (_current == null)
+                        {
+                            _current = new AmbientResource { Resource = ResourceFactory() };
+                        }
+                    }
+
+                    _current.AddReferenceCount(GetCallerKey());
+                    return _current;
+                }
+                throw new ArgumentNullException(nameof(ResourceFactory), nameof(ResourceFactory) + " must be initialized before calling " + nameof(Current));
+            }
+        }
+
+        private readonly object _referenceCountsLock = new object();
+        private readonly Dictionary<string, int> _referenceCounts = new Dictionary<string, int>();
+
+        protected IDisposable Resource;
+
+        protected AmbientResource() { }
+
+        private void AddReferenceCount(string caller)
+        {
+            lock (_referenceCountsLock)
+            {
+                if (!_referenceCounts.ContainsKey(caller))
+                {
+                    _referenceCounts[caller] = 0;
+                }
+                _referenceCounts[caller]++;
+            }
+        }
+
+        private void RemoveReferenceCount(string caller)
+        {
+            lock (_referenceCountsLock)
+            {
+                if (_referenceCounts.ContainsKey(caller))
+                {
+                    _referenceCounts[caller]--;
+
+                    if (_referenceCounts[caller] == 0)
+                    {
+                        _referenceCounts.Remove(caller);
+                    }
+                }
+            }
+        }
+
+        private static string GetCallerKey()
+        {
+            StackTrace stackTrace = new StackTrace();
+            MethodBase methodBase = stackTrace.GetFrame(2).GetMethod();
+            return methodBase.Name;
+        }
+
+        public void Dispose()
+        {
+            RemoveReferenceCount(GetCallerKey());
+            if (_referenceCounts.Count == 0)
+            {
+                Resource.Dispose();
+            }
+        }
+    }
+}
---
+++ AmbientResource/AmbientResource/GenericAmbientResource.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Timers;
+
+namespace Nicolai.Resources
+{
+    public abstract class GenericAmbientResource<TSelf, TResource> : IDisposable where TResource : IDisposable
+        where TSelf : GenericAmbientResource<TSelf, TResource>
+    {
+        private static readonly object currentLock = new object();
+        private static TSelf current;
+        private readonly Dictionary<string, int> referenceCounts = new Dictionary<string, int>();
+
+        private readonly object referenceCountsLock = new object();
+
+        protected TResource Resource;
+
+        private Timer timer;
+
+        public static Func<TResource> ResourceFactory { get; set; }
+
+        public static long ResourceTimeoutMillis { get; set; } = 30000;
+
+        public static TSelf Current
+        {
+            get
+            {
+                if (ResourceFactory != null)
+                {
+                    lock (currentLock)
+                    {
+                        if (current == null)
+                        {
+                            current = Activator.CreateInstance<TSelf>();
+                            current.Resource = ResourceFactory();
+                        }
+                        current.AddReferenceCount(GetCallerKey());
+                    }
+                    return current;
+                }
+                throw new ArgumentNullException(nameof(ResourceFactory),
+                    nameof(ResourceFactory) + " must be initialized before calling " + nameof(Current));
+            }
+        }
+
+        public void Dispose()
+        {
+            RemoveReferenceCount(GetCallerKey());
+            lock (this.referenceCountsLock)
+            {
+                if (this.referenceCounts.Count == 0)
+                {
+                    // Stop the timeout timer
+                    this.timer?.Stop();
+                    this.Resource.Dispose();
+                }
+            }
+        }
+
+        private void AddReferenceCount(string caller)
+        {
+            lock (this.referenceCountsLock)
+            {
+                if (!this.referenceCounts.ContainsKey(caller))
+                {
+                    this.referenceCounts[caller] = 0;
+                }
+                this.referenceCounts[caller]++;
+            }
+
+            // Restart timer
+            this.timer?.Stop();
+            this.timer = new Timer {AutoReset = false, Interval = ResourceTimeoutMillis};
+            this.timer.Elapsed += TimeoutTimerElapsed;
+            this.timer.Start();
+        }
+
+        private void TimeoutTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            lock (this.referenceCountsLock)
+            {
+                if (this.referenceCounts.Count > 0)
+                {
+                    this.referenceCounts.Clear();
+                    this.Resource.Dispose();
+                }
+            }
+        }
+
+        private void RemoveReferenceCount(string caller)
+        {
+            lock (this.referenceCountsLock)
+            {
+                if (this.referenceCounts.ContainsKey(caller))
+                {
+                    this.referenceCounts[caller]--;
+
+                    if (this.referenceCounts[caller] == 0)
+                    {
+                        this.referenceCounts.Remove(caller);
+                    }
+                }
+            }
+        }
+
+        private static string GetCallerKey()
+        {
+            var stackTrace = new StackTrace();
+            var methodBase = stackTrace.GetFrame(2).GetMethod();
+            return methodBase.Name;
+        }
+    }
+}
